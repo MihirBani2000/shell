2b72659873e186bb7a303544710afda6a796e9aa
MihirBani2000 <mihir.bani@research.iiit.ac.in>
2021-10-10 18:21:40 +0530

    added ~ in redirection

--- utils/redirect.c
+++ utils/redirect.c
@@ -8,7 +8,6 @@ int check_redirect()
     for (int i = 0; i < num_args; i++)
     {
         if (!strcmp(command[i], ">") || !strcmp(command[i], ">>") || !strcmp(command[i], "<"))
-            // if (command[i][0] == '>' || command[i][0] == '<')
             return 1;
     }
     return 0;
@@ -19,6 +18,7 @@ int handle_input_redir()
     int index_redir = -1;
     int fd_in = -1;
     char *temp1 = malloc(BIG_SIZE * sizeof(char));
+    char *input_file_name = malloc(BIG_SIZE * sizeof(char));
     char *input_file = malloc(BIG_SIZE * sizeof(char));
 
     for (int i = 0; i < num_args; i++)
@@ -26,7 +26,19 @@ int handle_input_redir()
         if (!strcmp(command[i], "<"))
         {
             index_redir = i;
-            strcpy(input_file, command[i + 1]);
+            strcpy(input_file_name, command[i + 1]);
+
+            // check for ~
+            if (input_file_name[0] == '~')
+            {
+                strcpy(input_file, HOME);
+                strcat(input_file, input_file_name + 1);
+            }
+            else
+            {
+                strcpy(input_file, input_file_name);
+            }
+            // printf("file: %s", input_file);
             if (input_file == NULL || !is_valid_file(input_file))
             {
                 fprintf(stderr, "redirection: Input File not found\n");
@@ -48,6 +60,7 @@ int handle_input_redir()
     dup2(fd_in, STDIN_FILENO);
     close(fd_in);
     free(input_file);
+    free(input_file_name);
 
     return 1;
 }
@@ -56,6 +69,7 @@ int handle_output_redir()
 {
     int index_redir = -1;
     int fd_out = -1;
+    char *output_file_name = malloc(BIG_SIZE * sizeof(char));
     char *output_file = malloc(BIG_SIZE * sizeof(char));
 
     for (int i = 0; i < num_args; i++)
@@ -63,7 +77,20 @@ int handle_output_redir()
         if (!strcmp(command[i], ">") || !strcmp(command[i], ">>"))
         {
             index_redir = i;
-            strcpy(output_file, command[i + 1]);
+            strcpy(output_file_name, command[i + 1]);
+
+            // check for ~
+            if (output_file_name[0] == '~')
+            {
+                strcpy(output_file, HOME);
+                strcat(output_file, output_file_name + 1);
+            }
+            else
+            {
+                strcpy(output_file, output_file_name);
+            }
+            // printf("file: %s", output_file);
+
             if (output_file == NULL)
             {
                 fprintf(stderr, "redirection: Output File name not given\n");
@@ -94,6 +121,7 @@ int handle_output_redir()
     dup2(fd_out, STDOUT_FILENO);
     close(fd_out);
     free(output_file);
+    free(output_file_name);
 
     return 1;
 }
